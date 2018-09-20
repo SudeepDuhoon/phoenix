6ffd21032ced90ba577cd77488149d74f265774c
sudeep <rishi.gothic@gmail.com>
2018-09-20 08:20:52 +0530

    Revert "set zstd algorith if zstd is enabled"
    This reverts commit ecb50b038ef249e3a167338cec2e9dc57b1c6f0e.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -40,11 +40,7 @@ static DEFINE_IDR(zram_index_idr);
 static DEFINE_MUTEX(zram_index_mutex);
 
 static int zram_major;
-#if IS_ENABLED(CONFIG_CRYPTO_ZSTD)
-	static const char *default_compressor = "zstd";
-#else
-	static const char *default_compressor = "lzo";
-#endif
+static const char *default_compressor = "lzo";
 
 /*
  * We don't need to see memory allocation errors more than once every 1
